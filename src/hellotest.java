6d6a5f850f4a43333db9dea4ea73f28ca9d10852
mmcclaren <mmcclaren@someemail.com>
2018-10-02 21:48:06 -0500

    initial commit

---
+++ src/hellotest.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class hellotest {
+
+    @Test
+    public void testNothing() {
+    }
+
+    @Test
+    public void testWillAlwaysFail() {
+        fail("An error message");
+    }
+
+}
