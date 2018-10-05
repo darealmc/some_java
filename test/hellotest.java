efa3d1a3818807a0f6df1287b6c27cfdbc3007b1
mmcclaren <mmcclaren@someemail.com>
2018-10-05 13:13:29 -0500

    setting comment on test fail

--- test/hellotest.java
+++ test/hellotest.java
@@ -10,9 +10,10 @@ public class hellotest {
     public void testNothing() {
     }
 
-    @Test
+/*    @Test
     public void testWillAlwaysFail() {
         fail("An error message");
     }
+*/
 
 }
